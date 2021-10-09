4d8414346c5a3c68886d8e605ce858c4b8c49266
Lucas Harada <lucasyharada@gmail.com>
2021-10-09 14:00:16 -0300

    apply black and isort formatting

--- airdrum_app/airdrum.py
+++ airdrum_app/airdrum.py
@@ -2,16 +2,16 @@
 import time
 
 import cv2
+import imutils
 import numpy as np
 import pyautogui
-import imutils
 
 
 def Press(key):
     pyautogui.press(key)
 
 
-cap = cv2.VideoCapture(0);
+cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
@@ -31,35 +31,145 @@ while True:
 
     # image/frame, start_point, end_point, color, thickness
     cv2.rectangle(frame, (0, 0), (200, 150), (255, 0, 0), 1)
-    cv2.putText(frame, 'RIDE', (70, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "RIDE",
+        (70, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (210, 0), (430, 150), (0, 0, 255), 1)
-    cv2.putText(frame, 'RIDE BELL', (245, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "RIDE BELL",
+        (245, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (440, 0), (650, 150), (255, 0, 0), 1)
-    cv2.putText(frame, 'HITHAT close', (445, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "HITHAT close",
+        (445, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (660, 0), (900, 150), (0, 0, 255), 1)
-    cv2.putText(frame, 'CRASH', (730, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "CRASH",
+        (730, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3,
+        cv2.LINE_AA,
+    )
 
     cv2.rectangle(frame, (0, 160), (50, 370), (255, 0, 0), 1)
-    cv2.putText(frame, 'SNARE', (10, 290), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "SNARE",
+        (10, 290),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (0, 380), (50, 570), (0, 0, 255), 1)
-    cv2.putText(frame, 'SNARE RIM', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "SNARE RIM",
+        (10, 500),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3,
+        cv2.LINE_AA,
+    )
 
     cv2.rectangle(frame, (850, 160), (900, 370), (255, 0, 0), 1)
-    cv2.putText(frame, 'HIT HAT', (770, 290), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "HIT HAT",
+        (770, 290),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (850, 380), (900, 570), (0, 0, 255), 1)
-    cv2.putText(frame, 'HIT HAT OPEN', (670, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "HIT HAT OPEN",
+        (670, 500),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3,
+        cv2.LINE_AA,
+    )
 
     cv2.rectangle(frame, (0, 580), (200, 700), (255, 0, 0), 1)
-    cv2.putText(frame, 'TOM HI', (50, 640), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "TOM HI",
+        (50, 640),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (210, 580), (430, 700), (0, 0, 255), 1)
-    cv2.putText(frame, 'TOM MID', (250, 640), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "TOM MID",
+        (250, 640),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (440, 580), (650, 700), (255, 0, 0), 1)
-    cv2.putText(frame, 'TOM LOW', (480, 640), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "TOM LOW",
+        (480, 640),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        3,
+        cv2.LINE_AA,
+    )
     cv2.rectangle(frame, (660, 580), (900, 700), (0, 0, 255), 1)
-    cv2.putText(frame, 'KICK', (740, 640), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(
+        frame,
+        "KICK",
+        (740, 640),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3,
+        cv2.LINE_AA,
+    )
 
     # for the red Object
-    contours, hierachy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierachy = cv2.findContours(
+        red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
     # startpoint, endpoint, color, thickness
     for cnt in contours:
@@ -67,53 +177,53 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         print((x, y))
         if x > 0 and y > 0 and x < 200 and y < 150:
-            Press('7')  # RIDE
+            Press("7")  # RIDE
 
             break
         if x > 210 and y > 0 and x < 430 and y < 150:
-            Press('8')  # RIDE BELL
+            Press("8")  # RIDE BELL
 
             break
         if x > 440 and y > 0 and x < 650 and y < 150:
-            Press('6')  # HIT HAT CLOSE
+            Press("6")  # HIT HAT CLOSE
 
             break
         if x > 660 and y > 0 and x < 900 and y < 150:
-            Press('9')  # CRASH
+            Press("9")  # CRASH
 
             break
 
         if x > 0 and y > 160 and x < 50 and y < 370:
-            Press('2')  # SNARE
+            Press("2")  # SNARE
 
             break
         if x > 0 and y > 380 and x < 50 and y < 570:
-            Press('3')  # SNARE RIM
+            Press("3")  # SNARE RIM
 
             break
         if x > 850 and y > 160 and x < 900 and y < 370:
-            Press('4')  # HIT HAT
+            Press("4")  # HIT HAT
 
             break
         if x > 850 and y > 380 and x < 900 and y < 570:
-            Press('5')  # HIT HAT OPEN
+            Press("5")  # HIT HAT OPEN
 
             break
 
         if x > 0 and y > 580 and x < 200 and y < 700:
-            Press('q')  # TOM HI
+            Press("q")  # TOM HI
 
             break
         if x > 210 and y > 580 and x < 430 and y < 700:
-            Press('w')  # TOM MID
+            Press("w")  # TOM MID
 
             break
         if x > 440 and y > 580 and x < 650 and x < 700:
-            Press('e')  # TOM LOW
+            Press("e")  # TOM LOW
 
             break
         if x > 660 and y > 580 and x < 900 and y < 700:
-            Press('1')  # HIT HAT OPEN
+            Press("1")  # HIT HAT OPEN
 
             break
         break
--- airdrum_app/range-detector.py
+++ airdrum_app/range-detector.py
@@ -7,9 +7,10 @@
 # or
 # (python) range-detector --filter HSV --webcam
 
-import cv2
 import argparse
 from operator import xor
+
+import cv2
 
 
 def callback(value):
@@ -28,21 +29,24 @@ def setup_trackbars(range_filter):
 
 def get_arguments():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-f', '--filter', required=True,
-                    help='Range filter. RGB or HSV')
-    ap.add_argument('-i', '--image', required=False,
-                    help='Path to the image')
-    ap.add_argument('-w', '--webcam', required=False,
-                    help='Use webcam', action='store_true')
-    ap.add_argument('-p', '--preview', required=False,
-                    help='Show a preview of the image after applying the mask',
-                    action='store_true')
+    ap.add_argument("-f", "--filter", required=True, help="Range filter. RGB or HSV")
+    ap.add_argument("-i", "--image", required=False, help="Path to the image")
+    ap.add_argument(
+        "-w", "--webcam", required=False, help="Use webcam", action="store_true"
+    )
+    ap.add_argument(
+        "-p",
+        "--preview",
+        required=False,
+        help="Show a preview of the image after applying the mask",
+        action="store_true",
+    )
     args = vars(ap.parse_args())
 
-    if not xor(bool(args['image']), bool(args['webcam'])):
+    if not xor(bool(args["image"]), bool(args["webcam"])):
         ap.error("Please specify only one image source")
 
-    if not args['filter'].upper() in ['RGB', 'HSV']:
+    if not args["filter"].upper() in ["RGB", "HSV"]:
         ap.error("Please speciy a correct filter.")
 
     return args
@@ -62,12 +66,12 @@ def get_trackbar_values(range_filter):
 def main():
     args = get_arguments()
 
-    range_filter = args['filter'].upper()
+    range_filter = args["filter"].upper()
 
-    if args['image']:
-        image = cv2.imread(args['image'])
+    if args["image"]:
+        image = cv2.imread(args["image"])
 
-        if range_filter == 'RGB':
+        if range_filter == "RGB":
             frame_to_thresh = image.copy()
         else:
             frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -77,31 +81,35 @@ def main():
     setup_trackbars(range_filter)
 
     while True:
-        if args['webcam']:
+        if args["webcam"]:
             ret, image = camera.read()
 
             if not ret:
                 break
 
-            if range_filter == 'RGB':
+            if range_filter == "RGB":
                 frame_to_thresh = image.copy()
             else:
                 frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(
+            range_filter
+        )
 
-        thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+        thresh = cv2.inRange(
+            frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max)
+        )
 
-        if args['preview']:
+        if args["preview"]:
             preview = cv2.bitwise_and(image, image, mask=thresh)
             cv2.imshow("Preview", preview)
         else:
             cv2.imshow("Original", image)
             cv2.imshow("Thresh", thresh)
 
-        if cv2.waitKey(1) & 0xFF is ord('q'):
+        if cv2.waitKey(1) & 0xFF is ord("q"):
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
