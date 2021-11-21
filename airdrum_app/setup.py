fb85362bf945df94099d33d205558556d7281dad
João Victor Oliveira <joao.oliveira@controlid.com.br>
2021-11-21 13:50:54 -0300

    add requirements and setup.py

---
+++ airdrum_app/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='airdrum_app',
+    version='0.0.1',
+    packages=['drum'],
+    url='https://github.com/pedrofbo/airdrum-app',
+    license='MIT',
+    author='joao',
+    author_email='joao.marques.oliveira@usp.br',
+    description='Aplicação de airdrum, com o uso do OpenCV'
+)
